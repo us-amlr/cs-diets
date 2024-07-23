8d186f65c7739954fc56041a9a6c7f2c2b778009
Fanny Sanchez Villarreal <fannyruby7475@gmail.com>
2024-07-23 15:54:23 -0700

    Reviewed to be sent to TESTdb

--- AFS_DietData_2010-11.R
+++ AFS_DietData_2010-11.R
@@ -43,6 +43,7 @@ SC2010_11 <- SC2010_11_ORIG %>%
          krill_type = if_else(krill_type == "Y", "Yes", "No"), 
          fish_type = if_else(fish_type == "Y", "Yes", "No"), 
          squid_type = if_else(squid_type == "Y", "Yes", "No"), 
+         female_id = if_else(female_id == "None", NA, female_id),
          location = if_else(location == "Vei?" | location == "NS", NA, location),
          # collection_date = as.Date(collection_date), 
          collection_date = case_when(
@@ -52,14 +53,51 @@ SC2010_11 <- SC2010_11_ORIG %>%
          process_date = as.Date(process_date), 
          processor = NA_character_, #str_sub(observer_code, 1, 3), 
          collector = NA_character_, 
+         tag = str_pad(as.numeric(female_id), width = 3, pad = "0", side = "left"),
          carapace_save = case_when(
            is.na(notes) ~ 0, 
            str_detect(tolower(notes), "carapaces saved") ~ 1, 
            .default = 0)) %>%
-  select(sample_num: squid_type, collector, notes: carapace_save)
+  select(sample_num: squid_type, collector, notes: carapace_save) %>% 
+  mutate_location()
 
 
-#NS values are marked as NA so do a case when for specific coll-date (cell#) 
+table(SC2010_11$sample_num, useNA = "ifany")
+table(SC2010_11$sample_type, useNA = "ifany")
+table(SC2010_11$species, useNA = "ifany")
+table(SC2010_11$sex, useNA = "ifany")
+table(SC2010_11$collection_date, useNA = "ifany")
+table(SC2010_11$krill_type, useNA = "ifany")
+table(SC2010_11$squid_type, useNA = "ifany")
+table(SC2010_11$fish_type, useNA = "ifany")
+table(SC2010_11$carapace_save, useNA = "ifany")
+
+table(SC2010_11$fish_type, SC2010_11$squid_type, SC2010_11$krill_type,
+      useNA = "ifany")
+sum(duplicated(SC2010_11$sample_num)) == 0
+
+
+beaches <- read.csv(here("reference_tables/beaches.csv")) %>% 
+  select(beach_id = ID, location = name)
+observers <- read.csv(here("reference_tables/observers.csv"))
+tags <- read.csv(here("reference_tables/tags.csv")) %>% 
+  filter(tag_species == "Fur seal", tag_type != "U-tag") %>% 
+  select(tag_id = ID, tag, species = tag_species)
+
+all(is.na(SC2010_11$location) | (SC2010_11$location %in% beaches$location))
+all(is.na(SC2010_11$collector) | (SC2010_11$collector %in% observers$observer))
+all(is.na(SC2010_11$processor) | (SC2010_11$processor%in% observers$observer))
+
+SC2010_11$location[!(is.na(SC2010_11$location) | (SC2010_11$location %in% beaches$location))]
+
+diets2010_11_todb <- SC2010_11 %>%
+  left_join(beaches, by = join_by(location)) %>%
+  left_join(tags, by = join_by(species, tag)) %>%
+  select(-c(location, tag, female_id, observer_code)) %>%
+  relocate(sample_type, species: tag_id, .before = notes)
+
+
+#NS values mark them as NA so do a case when for specific coll-date (cell#) 
 #and change it to its respective date
 #Notes on renaming columns---------------------
 # View(SC2010_11)
