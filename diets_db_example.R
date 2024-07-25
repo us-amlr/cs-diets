8c35275fa69de204f5f8d1010d5f5ea8e364e5ef
Fanny Sanchez Villarreal <fannyruby7475@gmail.com>
2024-07-25 15:09:57 -0700

    First plot

--- diets_db_example.R
+++ diets_db_example.R
@@ -1,9 +1,24 @@
 library(dplyr)
 library(odbc)
 library(here)
+library(tidyverse)
 
 con <- dbConnect(odbc(), filedsn = here("dsn/amlr-pinniped-db-test.dsn"))
 
 diets <- tbl(con, "vDiets") %>% collect()
 
-diets %>% glimpse()
+diets.scat <- diets %>% 
+  filter(sample_type %in% c("scat", "Scat"))
+
+ggplot(diets, aes(x = season_name)) +
+  geom_bar()
+
+xy <- ggplot(diets, aes(x = season_name)) +
+  geom_bar(fill = "brown") 
+
+xy + scale_x_discrete(guide = guide_axis(angle = 90)) +
+  ggtitle(label = "Scat Collections per Season") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab(label = "Season Year") + ylab(label = "Number of Scats") +
+  scale_y_continuous(limits = c(0, 140),
+                     breaks = seq(0, 140, 20))
